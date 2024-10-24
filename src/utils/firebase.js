c516244d40c0d1033d166313489334add1ab0713
Hansraj <hraj2662001@gmail.com>
2024-10-24 20:58:05 +0530

    firebase setup

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,6 +8,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyCubwK5wqoxmNAdY-s9Q0z-4gmwvW5QwfY",
   authDomain: "netflixgpt-bb105.firebaseapp.com",
